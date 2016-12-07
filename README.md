4f0d2af2a7d05d2401d5efbb93a0e8c01e67e50f
MarcusHogue <marcus@hogue.me>
2016-12-07 11:02:42 -0600

    Initial working version
    So kludgy, will add full functionality, such as response modification
    in v2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # znc-ducker
+
+This is such a hack and barely works.  I'm sorry to any channels this gets deployed in.
--- ducker.py
+++ ducker.py
@@ -13,66 +13,35 @@
 #  limitations under the License.
 #
 #  duck regex match : \\_o<|\\_O<|\\_0<|\\_\u00f6<|\\_\u00f8<|\\_\u00f3<
-#  Work in progress using Thor77's SlapAnswer as the starting point
 
 """A module for ZNC (IRC bouncer software)
-Purpose: auto-shoot (or befriend) gonzobot ducks
+Purpose: React, auto-shoot, or befriend gonzobot ducks
 """
-import random
-import json
+# ducker.py
+
 import znc
 import re
-
+import random
 
 class ducker(znc.Module):
-    description = 'Do things to ducks automatically'
-    module_types = [znc.CModInfo.NetworkModule]
+    description = "Example python3 module for ZNC"
 
-    def OnLoad(self, args, message):
-        self.default_answers = [
-            '.bef','.bang','.befriend','.bnag','.bfe',
+    def OnChanMsg(self, nick, channel, message):
+        self.responses = [
+            'What is that thing?','Is that a duck?','I\'m scared of ducks','Oh, that one looks friendly!','Kill it with fire!','.bef','.bang',
         ]
-        if 'answers' in self.nv:
-            self.ANSWERS = json.loads(self.nv['answers'])
-        else:
-            self.ANSWERS = self.default_answers
-            self.save_answers()
-        return True
-
-    def OnModCommand(self, cmd):
-        split = cmd.split()
-        command = str(split[0]).lower()
-        args = [a.lower() for a in split[1:]]
-        if command == 'help':
-            self.command_help()
-        elif command == 'list':
-            self.command_list()
-
-    def save_answers(self):
-        self.nv['answers'] = json.dumps(self.ANSWERS)
-
-    def command_help(self):
-        self.PutModule('list | get a list with msgs')
-        return True
-
-    def command_list(self):
-        for index, value in enumerate(self.ANSWERS):
-            self.PutModule('{} | {}'.format(index, value))
-        return True
-
-    def OnChanAction(self, invoker, channel, message):
-        own_nick = self.GetNetwork().GetIRCNick().GetNick()
         own_host = self.GetNetwork().GetIRCNick().GetHostMask()
-        nick = invoker.GetNick()
-        channel = channel.GetName()
-        duckregex = '\\_o<|\\_O<|\\_0<|\\_\u00f6<|\\_\u00f8<|\\_\u00f3<'
+#        msg = 'PRIVMSG #{channel} :{msg}'.format(channel=channel, msg=msg)
+#        duckregex = re.compile(ur'[.*\\_o<.*|.*\\_O<.*|.*\\_0<.*|.*\\_ö<.*|.*\\_ø<.*|.*\\_ó<.*]')
+        duckregex = '.*\\_o<.*|.*\\_O<.*|.*\\_0<.*'
+#        duckregex = re.compile('[.*\\_ó<.*]')
+#        duckregex = '.*\\_o<.*'
+#        duckregex = '.*hi.*'
         msg = str(message)
-        if re.match(duckregex, msg) is not None
-            self.get_duck(channel, nick, own_host)
+        if re.match(duckregex, msg) is not None:
+             response = random.choice(self.responses)
+             self.GetNetwork().PutIRC("PRIVMSG {0} :{1}".format(channel.GetName(), response))
+             self.GetNetwork().PutUser(':{own_host} {msg}'.format(own_host=own_host, msg=msg))
+             self.PutModule("Hey, {0} said {1} on {2}".format(nick.GetNick(), message.s, channel.GetName()))
+             self.PutModule("I said \"{0}\" in response".format(response))
         return znc.CONTINUE
-
-    def get_duck(self, channel, nick, own_host):
-        msg = random.choice(self.ANSWERS)
-        msg = 'PRIVMSG {channel} :{msg}'.format(channel=channel, msg=msg)
-        self.GetNetwork().PutIRC(msg)
-        self.GetNetwork().PutUser(':{own_host} {msg}'.format(own_host=own_host, msg=msg))
