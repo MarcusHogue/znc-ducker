8207cf5cd27e6bedca68a05f2265079a667763d2
MarcusHogue <marcus@hogue.me>
2016-12-07 16:56:52 -0600

    Just get rid of hidden spaces

--- ducker.py
+++ ducker.py
@@ -32,8 +32,9 @@ class ducker(znc.Module):
             'What is that thing?','Is that a duck?','I\'m scared of ducks','Oh, that one looks friendly!','Kill it with fire!','.bef','.bang',
         ]
         own_host = self.GetNetwork().GetIRCNick().GetHostMask()
-        duck_re = re.compile('[o○O0öøóóȯôőŏᴏōο]\u200b*(<|>|＜)')
+        duck_re = re.compile('[o○O0öøóóȯôőŏᴏōο](<|>|＜)')
         msg = str(message)
+        msg = msg.replace('\u200b', '')
         if duck_re.search(msg) is not None:
              self.PutModule("INCOMING!")
              response = random.choice(self.responses)
