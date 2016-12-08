8af917d4f6f3e5e6a75c5bbba334fac924698aaa
MarcusHogue <marcus@hogue.me>
2016-12-08 09:45:18 -0600

    Specify the incoming channel

--- ducker.py
+++ ducker.py
@@ -56,7 +56,7 @@ class ducker(znc.Module):
         msg = str(message)
         msg = msg.replace('\u200b', '')
         if nick.GetNick() == botname and duck_re.search(msg) is not None:
-             self.PutModule("INCOMING!")
+             self.PutModule("INCOMING IN #{}!".format(channel.GetName()))
              response = random.choice(self.responses)
              delay = random.randint(0,99)/10+1
              time.sleep(delay)
