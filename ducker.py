3b4e2f2a7002b0f5786330dee3a61cbd7284eeee
MarcusHogue <marcus@hogue.me>
2016-12-08 22:47:41 -0600

    Making things more "functional"

--- ducker.py
+++ ducker.py
@@ -1,3 +1,4 @@
+#  ducker.py
 #  Copyright 2016 Marcus Hogue <marcus@hogue.me>
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,8 +18,6 @@
 """A module for ZNC (IRC bouncer software)
 Purpose: React, auto-shoot, or befriend gonzobot ducks
 """
-# ducker.py
-
 import znc
 import re
 import random
@@ -28,7 +27,7 @@ class ducker(znc.Module):
     description = "Gonzobot duck autoresponder for ZNC"
     module_types = [znc.CModInfo.NetworkModule]
 
-    def OnChanMsg(self, nick, channel, message):
+    def OnLoad(self, args, message):
         self.responses = [
             'What is that thing?','Is that a duck?',
             'I\'m scared of ducks','Oh, that one looks friendly!',
@@ -52,22 +51,29 @@ class ducker(znc.Module):
             '^(;,;)^',
         ]
         botname = "gonzobot"
-        own_host = self.GetNetwork().GetIRCNick().GetHostMask()
-        duck_re = re.compile('[o○O0öøóóȯôőŏᴏōο](<|＜)')
         decoy = 'DECOY DUCK'
+        duck_re = re.compile('[o○O0öøóóȯôőŏᴏōο](<|＜)')
+
+    def OnChanMsg(self, nick, channel, message):
+        own_host = self.GetNetwork().GetIRCNick().GetHostMask()
+        channel = channel.GetName())
+        nick = nick.GetNick()
         msg = str(message)
         msg = msg.replace('\u200b', '')
-        if nick.GetNick() == botname and duck_re.search(msg) is not None:
-             self.PutModule("INCOMING IN {}!".format(channel.GetName()))
-             if msg.find(decoy) != -1:
-                self.PutModule("(I think it's a DECOY)")
-                response = 'nice try.'
-             else:
-                response = random.choice(self.responses)
-             delay = random.randint(0,99)/10+1
-             time.sleep(delay)
-             self.GetNetwork().PutIRC("PRIVMSG {0} :{1}".format(channel.GetName(), response))
-             self.PutModule("Triggered when {0} said {1} on {2}".format(nick.GetNick(), message.s, channel.GetName()))
-             self.PutModule("I waited {1} seconds and said \"{0}\" in response".format(response, delay))
-             self.GetNetwork().PutUser(':{own_host} {msg}'.format(own_host=own_host, msg=response))
+        if nick == botname and duck_re.search(msg) is not None:
+            self.duck_react(msg, channel, nick, own_host)
         return znc.CONTINUE
+
+    def duck_react(self, msg, channel, nick, own_host)
+        self.PutModule("INCOMING IN {}!".format(channel)
+        if msg.find(decoy) != -1:
+            self.PutModule("(I think it's a DECOY)")
+            response = 'nice try.'
+        else:
+            response = random.choice(self.responses)
+        delay = random.randint(0,99)/10+1
+        time.sleep(delay)
+        self.GetNetwork().PutIRC("PRIVMSG {0} :{1}".format(channel, response))
+        self.PutModule("Triggered when {0} said {1} on {2}".format(nick, message.s, channel)
+        self.PutModule("I waited {1} seconds and said \"{0}\" in response".format(response, delay))
+        self.GetNetwork().PutUser(':{own_host} {msg}'.format(own_host=own_host, msg=response))
