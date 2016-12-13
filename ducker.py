be48c7c317bdd372c5492774c4e75211fc1ac89a
MarcusHogue <marcus@hogue.me>
2016-12-13 10:58:56 -0600

    Rolling back to working version to introduce threading.

--- ducker.py
+++ ducker.py
@@ -16,73 +16,56 @@
 """A module for ZNC (IRC bouncer software)
 Purpose: React, auto-shoot, or befriend gonzobot ducks
 """
+
 import znc
 import re
 import random
 import time
-import threading
 
 class ducker(znc.Module):
     description = "Gonzobot duck autoresponder for ZNC"
     module_types = [znc.CModInfo.NetworkModule]
-    # TODO:
-    # move all this crap to OnLoad
-    # add toggles for hardcore
-    # add filters for channels
-    # add toggle for friend or bang preference
-    # add toggle to EVER friend or bang
-    # add config for timer (can go short if not shooting, right?)
-    responses = [
-        'What is that thing?','Is that a duck?',
-        'I\'m scared of ducks','Oh, that one looks friendly!',
-        'Kill it with fire!','.bef',
-        '.bang','.bfe',
-        '.bnag','bang',
-        'bef','.befriend',
-        'I\'m not feeling this one','I need coffee',
-        'My cousin was bitten by a duck once','/me runs away',
-        '¯\\_(ツ)_/¯','.lenny',
-        '\\o/','.flip DUCK!',
-        '.ask Is that a duck?','ლ(ಠ益ಠ)ლ',
-        '༼ ༎ຶ ෴ ༎ຶ༽','「(°ヘ°)',
-        'ᕕ( ᐛ )ᕗ','(╯°□°）╯︵ ┻━┻',
-        '༼ つ ◕_◕ ༽つ','(✿◠‿◠)',
-        '¯(°_o)/¯','(͡° ͜ʖ ͡°)',
-        '(ಠ_ಠ)','(╯_╰)',
-        '(─‿‿─)','\,,/(^_^)\,,/',
-        '(¬､¬)','(ﾉﾟ0ﾟ)ﾉ',
-        '( •_•)O*¯`·.¸.·´¯`°Q(•_• )',
-        '^(;,;)^','TRIGGERED',
-    ]
-    botnames = ['gonzobot','slaybot']
-    decoy = 'DECOY DUCK'
-    duck_re = re.compile('[o○O0öøóóȯôőŏᴏōο](<|＜)')
-
-    def OnLoad(self, args, message):
-        return True
 
     def OnChanMsg(self, nick, channel, message):
-        own_host = self.GetNetwork().GetIRCNick().GetHostMask()
-        channel = channel.GetName()
-        nick = nick.GetNick()
+        self.responses = [
+            'What is that thing?','Is that a duck?',
+            'I\'m scared of ducks','Oh, that one looks friendly!',
+            'Kill it with fire!','.bef',
+            '.bang','.bfe',
+            '.bnag','bang',
+            'bef','.befriend',
+            'I\'m not feeling this one','I need coffee',
+            'My cousin was bitten by a duck once','/me runs away',
+            '¯\\_(ツ)_/¯','.lenny',
+            '\\o/','.flip DUCK!',
+            '.ask Is that a duck?','ლ(ಠ益ಠ)ლ',
+            '༼ ༎ຶ ෴ ༎ຶ༽','「(°ヘ°)',
+            'ᕕ( ᐛ )ᕗ','(╯°□°）╯︵ ┻━┻',
+            '༼ つ ◕_◕ ༽つ','(✿◠‿◠)',
+            '¯(°_o)/¯','(͡° ͜ʖ ͡°)',
+            '(ಠ_ಠ)','(╯_╰)',
+            '(─‿‿─)','\,,/(^_^)\,,/',
+            '(¬､¬)','(ﾉﾟ0ﾟ)ﾉ',
+            '( •_•)O*¯`·.¸.·´¯`°Q(•_• )',
+            '^(;,;)^',
+        ]
+        botnames = ['gonzobot','slaybot']
+#        own_host = self.GetNetwork().GetIRCNick().GetHostMask()
+        duck_re = re.compile('[o○O0öøóóȯôőŏᴏōο](<|＜)')
+        decoy = 'DECOY DUCK'
         msg = str(message)
         msg = msg.replace('\u200b', '')
         for bot in botnames:
-            if nick == bot and duck_re.search(msg) is not None:
-                threading.start(self.duck_react(msg, channel, nick, own_host))
+           if nick.GetNick() == bot and duck_re.search(msg) is not None:
+             self.PutModule("INCOMING IN {}!".format(channel.GetName()))
+             if msg.find(decoy) != -1:
+                self.PutModule("(I think it's a DECOY)")
+                response = 'nice try.'
+             else:
+                response = random.choice(self.responses)
+             delay = random.randint(0,99)/10+1
+             time.sleep(delay)
+             self.GetNetwork().PutIRC("PRIVMSG {0} :{1}".format(channel.GetName(), response))
+             self.PutModule("Triggered when {0} said {1} on {2}".format(nick.GetNick(), message.s, channel.GetName()))
+             self.PutModule("I waited {1} seconds and said \"{0}\" in response".format(response, delay))
         return znc.CONTINUE
-
-    def duck_react(self, msg, channel, nick, own_host):
-        self.PutModule("INCOMING IN {}!".format(channel))
-        if msg.find(decoy) != -1:
-            self.PutModule("(I think it's a DECOY)")
-            response = 'nice try.'
-        else:
-            response = random.choice(self.responses)
-        delay = random.randint(0,99)/10+1
-        time.sleep(delay)
-        self.GetNetwork().PutIRC("PRIVMSG {0} :{1}".format(channel, response))
-        self.PutModule("Triggered when {0} said {1} on {2}".format(nick, message.s, channel))
-        self.PutModule("I waited {1} seconds and said \"{0}\" in response".format(response, delay))
-        self.GetNetwork().PutUser(':{own_host} {msg}'.format(own_host=own_host, msg=response))
-        return True
